8bf7e549198006ab57065d7976989916715efb5a
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-04-29 16:22:42 +0200

    fine milestone 1

--- js/script.js
+++ js/script.js
@@ -2,13 +2,29 @@ var app = new Vue (
     {
         el: '#root',
         data: {
-            
+            // Array che viene popolato dalla chiamata all'API
+            searchedMovies: [],
+
+            // Ricerca dell'utente nella input
+            userSearch: '',
         },
         methods: {
-
-        },
-        mounted () {
-
+            // Funzione che manda una chiamata all'API quando l'utente preme il button o preme enter
+            searchMovie() {
+                if ( this.userSearch != '' ) {
+                    axios
+                        .get('https://api.themoviedb.org/3/search/movie', {
+                            params: {
+                                api_key: '86aa4c6e9326e99a2398f49eac5855b2',
+                                query: this.userSearch
+                            }
+                        })
+                        .then( (response) => {
+                            const result = response.data;
+                            this.searchedMovies = result.results;
+                        });
+                }
+            }
         }
     }
 );
