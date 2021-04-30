d114267fec53862542d0ac20262058c385ff634f
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-04-30 11:20:21 +0200

    inizio funzione per aggiungere le serie tv

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ var app = new Vue (
             // Array che viene popolato dalla chiamata all'API
             searchedMovies: [],
 
+            searchedSeries: [],
+
             // Ricerca dell'utente nella input
             userSearch: '',
 
@@ -15,24 +17,38 @@ var app = new Vue (
                 fr: 'https://lipis.github.io/flag-icon-css/flags/4x3/fr.svg',
                 de: 'https://lipis.github.io/flag-icon-css/flags/4x3/de.svg',
                 es: 'https://lipis.github.io/flag-icon-css/flags/4x3/es.svg'
-            }
+            },
+
+            apiKey: '86aa4c6e9326e99a2398f49eac5855b2'
         },
         methods: {
             // Funzione che manda una chiamata all'API quando l'utente preme il button o preme enter
-            searchMovie() {
+            searchMovieAndSeries() {
                 if ( this.userSearch != '' ) {
-                    axios
-                        .get('https://api.themoviedb.org/3/search/movie', {
-                            params: {
-                                api_key: '86aa4c6e9326e99a2398f49eac5855b2',
-                                query: this.userSearch
-                            }
-                        })
-                        .then( (response) => {
-                            const result = response.data;
-                            this.searchedMovies = result.results;
-                        });
+                    this.callApi('https://api.themoviedb.org/3/search/movie', this.searchedMovies);
+                    console.log(this.searchedMovies);
+
+                    this.callApi('https://api.themoviedb.org/3/search/tv', this.searchedSeries);
+                    console.log(this.searchedSeries);
                 }
+            },
+
+            // Funzione che effettua una chiamata all'API
+            // 
+            // endpoint --> endpoint per effettuare la chiamata
+            callApi(endpoint, array) {
+                axios
+                    .get( endpoint , {
+                        params: {
+                            api_key: this.apiKey,
+                            query: this.userSearch
+                        }
+                    })
+                    .then( (response) => {
+                        const result = response.data;
+                        array = result.results;
+                        console.log('array', array);
+                    });
             }
         }
     }
